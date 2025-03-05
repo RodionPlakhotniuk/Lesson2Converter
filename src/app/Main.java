0da67a73f7b8eb46d679003acaa3d5403f5eb466
RodionPlakhotniuk <raidertwk@gmail.com>
2025-03-05 22:21:04 +0200

    added conversion of milesToKilometers and output

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,8 +1,18 @@
 package app;
 
 public class Main {
+
+    private static final double CONV_M = 1.60934;
+
     public static void main(String[] args) {
+        double miles = 15;
 
         System.out.println("Привіт, я Convi бот для конвертації одиниць виміру");
+        System.out.printf("%d миль = %f кілометри\n", (int) miles, convMilesToKilometrs(miles));
+
+    }
+
+    private static double convMilesToKilometrs(double miles) {
+        return miles * CONV_M;
     }
 }
